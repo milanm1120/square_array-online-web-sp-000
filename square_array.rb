76a394e59b06aa89a6a20ef8479ebf5bc243386e
Milan Mody <milanm1120@gmail.com>
2021-02-22 20:39:17 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,7 @@
-def square_array(array)
-  new_array = []
-  array.each do |x|
-    new_array << x**2
+def square_array(numbers)
+  new_array=[]
+  numbers.each do |number|
+    new_array<<number**2
   end
-return new_array
+  return new_array
 end
